5b96c5523db75804856d86a0690d76f4d1a15333
user <user@users-MacBook-Air.local>
2016-09-16 09:19:29 +0100

    added detail view, extra detail needed

---
+++ pokeDex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  pokeCell.swift
+//  pokeDex
+//
+//  Created by user on 12/09/2016.
+//  Copyright © 2016 David Kennedy. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokeDexId)")
+    }
+}
